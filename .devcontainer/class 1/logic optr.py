0e2c5118c5de9375ab758ac4ee04551a2e7521ac
Hritik jha <144478520+Jhahritik95@users.noreply.github.com>
2023-09-18 06:00:38 +0000

    my task

--- .devcontainer/class 1/logic optr.py
+++ .devcontainer/class 1/logic optr.py
@@ -2,3 +2,4 @@ x=10
 y=20
 print(x==10 and x<y and x==y)
 print(x==10 or x<y or x==y )
+print(x!=10 or x<y or x==y)
---
+++ .devcontainer/class 1/m.py
@@ -0,0 +1,5 @@
+string2="Hello"
+print('e' in string2)
+
+L=[10,20,30,40]
+print(50 not in L)
